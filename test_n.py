146857bbf4b8add49f6bbfde8dc48c20aaf6ec53
naleendhanushka <naleendhanushka@gmail.com>
2015-12-03 14:48:45 +0530

    modified test_n.py
    Signed-off-by: naleendhanushka <naleendhanushka@gmail.com>

--- test_n.py
+++ test_n.py
@@ -1,56 +1,35 @@
 # -*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
-import wave
 import segmenter
 import classifier
 import corrector
-import synthesizer
+# import synthesizer
 
 # input_text=u" සංවිටාතවලිනුක්, 123 ඉල්ලා සිවින? amali ග�තක් කො�ඹ ඡලයa ඉහළන්  "
-def run(image_path):
-	image_path_ = image_path.rsplit('.', 1)[0]
-	preprocess_image_path = image_path_+'_bw.jpg'
-	audio_path = image_path_+'.wav'
 
-	image = cv2.imread(image_path)
+image = cv2.imread('ww - Copy.jpg')
 
-	image_bw = segmenter.preprocess(image)
+image_bw = segmenter.preprocess(image)
 
-	cv2.imwrite(preprocess_image_path, image_bw)
+lines = segmenter.segment_lines(image_bw)
 
-	lines = segmenter.segment_lines(image_bw)
+classified_text = ""
 
-	classified_text = ""
+for i, line in enumerate(lines[:1]):
+	character_images = segmenter.segment_line(line, i)
+	for character_image in character_images:
+		classified_text += classifier.classify(character_image)
+		#classified_text += "2"
 
-	for i, line in enumerate(lines[:1]):
-		character_images = segmenter.segment_line(line, i)
-		for character_image in character_images:
-			classified_text += classifier.classify(character_image)
-			#classified_text += "2"
+# remove extra spaces
+classified_text = classified_text.strip()
+classified_text = " ".join(classified_text.split())
 
-	# remove extra spaces
-	classified_text = classified_text.strip()
-	classified_text = " ".join(classified_text.split())
+print "classified text : " + classified_text
 
-	# corrected_text = corrector.correct(classified_text)
-	corrected_text = classified_text
-	# "ස්ථානාධිපති"
-	print corrected_text
+# corrected_text = corrector.correct(classified_text)
+#print "corrected text : " + corrected_text
 
-	synthesized_data = synthesizer.synthesize("ස")
-
-	# print "classified text : " + classified_text
-
-	audio_outfile = wave.open(audio_path, 'wb')
-
-	audio_outfile.setparams(synthesized_data[0][0])
-
-	for i in range(0, len(synthesized_data), 1):
-			audio_outfile.writeframes(synthesized_data[i][1])
-
-	# #print "corrected text : " + corrected_text
-
-
-
-	return classified_text, preprocess_image_path, audio_path
+# synthesized_voice = synthesizer.synthesize(corrected_text)
