9fcf8664b78178af754264a9c3e90cc7ee250642
unknown <amali.upendra92@gmail.com>
2015-12-07 14:21:23 +0530

    text changed

--- test_a.py
+++ test_a.py
@@ -7,8 +7,14 @@ import numpy as np
 import corrector
 # import synthesizer
 
+
+file = open('corrector/input.txt', 'r')
+input_text = file.read()
+# input_text = raw_text[3:]
+print input_text
 # input_text=" රජගහා විහාරෆ හඤූතර තරඔක් කදූභැටියරී වර පසින් ඇහ් අ "
-input_text="රජගහා විහාරෆ හඤූතර තරඔක් කදූභැටිය වර පසින් ඇහ් අ මිනාගය සංමිටානය නඳුවැටිය"
+# input_text="මම රජගහා විහාරෆ හඤූතර තරඔක් කදූභැටිය වර පසින් ඇහ් අ මිනාගය සංමිටානය නඳුවැටිය ගියෙමි සමවන්ධ කෙෂ්ත්‍රඵලය තොරකුරු තාක්ෂණ කඨය  "
+# input_text="රට පුරා මේ වන විට උග්‍ර ‍පොහොර හිඟයක් පවතින බවත්, මේ හේතුවෙන් ගොවි  ජනතව දැඩි දුෂ්කරතාවකට පත්ව සිටින බවත් ප්‍රධායා"
 
 corrected_text = corrector.correct(input_text)
 # corrected_text=corrected_text.split(" ")
